a2151e2870f7c637b38235e10750499cfac8e280
CarlaNunes <carla.nunes@usp.br>
2020-11-28 21:19:25 -0300

    Update escreve_Binario2.c

--- escreve_Binario2.c
+++ escreve_Binario2.c
@@ -10,80 +10,48 @@
 //Também com preenchimento de campos "pequenos com lixo"
 void escreve_Binario2(FILE *arquivo, int cont,char nomeArquivoBinario[20]){
   
-  char status = '0';
-  char nome[40];
+  char status = '0',removido;
+  char grau[3],data1[10],data2[10];
   int quantidadePessoas = cont;
   char lixo[] = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
 
   if ((arquivo = fopen(nomeArquivoBinario, "wb")) == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Falha na abertura do arquivo.\n");
     }
      // fprintf(arquivo,"%s%d%s",&status,quantidadePessoas,lixo);
      fwrite(&status,sizeof(char) ,1, arquivo);
      fwrite(&quantidadePessoas,sizeof(int) ,1, arquivo);
      fwrite(&lixo, sizeof(char) ,1, arquivo);
 
+  else{
     for(int i = 0; i<cont; i++){
-      //Completando as strings com lixo
-      /*
-      if(strlen(pessoa[i].pessoa)<40){
-          int tamFinal = 40 - strlen(pessoa[i].pessoa);
-          for(int j = tamFinal; j < 40; j++){
-            strcpy(pessoa[i].pessoa, "$"); 
-          }
-         
-        //fwrite(&pessoa, sizeof(Pessoa),1,arquivo);  
-      }
-      if(strlen(pessoa[i].twitterPessoa)<15){
-          int tamFinal = 15 - strlen(pessoa[i].pessoa);
-          for(int j = tamFinal; j < 15;j++){
-            strcpy(pessoa[i].twitterPessoa, "$"); 
-          }
-      }
-      */
-      //else{
+      
             int tam = 0, tam2 = 0;
-            pessoa[i].removido = '1';
-            fwrite(&pessoa[i].removido,1,sizeof(char),arquivo);
-            int id = pessoa[i].idPessoa;
+            removido = '1';
+            dados[i].removido = '1';
+            fwrite(&removido,1,sizeof(char),arquivo);
+            int id = dados[i].idPessoaQueSegue;
             fwrite(&id,1,sizeof(int),arquivo);
 
-          //  if(strlen(pessoa[i].pessoa)<1){
-         // int tamFinal = 40 - strlen(pessoa[i].pessoa);
-         // for(int j = tamFinal; j < 40;j++){
-           //   char lixo = '$';
-             //  strcpy(pessoa[i].pessoa, "$"); 
-              // strcpy(nome,pessoa[i].pessoa);
-              // fwrite(&nome,1,sizeof(char),arquivo);
-             
-           // }
+            int id2 = dados[i].idPessoaQueESeguida;
+            fwrite(&id2,1,sizeof(int),arquivo);
            
-            strcpy(nome,pessoa[i].pessoa);
-            fwrite(&nome,1,sizeof(char),arquivo);
-            int idade = pessoa[i].idade;
-            fwrite(&idade,1,sizeof(int),arquivo);
-            char twitter[15];
-            strcpy(twitter,pessoa[i].pessoa);
-            fwrite(&twitter,1,sizeof(char),arquivo);
-            //fwrite(&pessoa,1, sizeof(Pessoa),arquivo);
-            
-        
-       //}
-      // if(strlen(pessoa[i].pessoa)<1){
-         // int tamFinal = 40 - strlen(pessoa[i].pessoa);
-         // for(int j = tamFinal; j < 40;j++){
-         //   char lixo = '$';
-          //  strcpy(pessoa[i].pessoa, "$"); 
+            strcpy(grau,dados[i].grauAmizade);
+            fwrite(&grau,1,sizeof(char),arquivo);
 
-         //   fwrite(&pessoa,sizeof(Pessoa),1,arquivo);
-        //  }
-           
+            strcpy(data1,dados[i].dataInicioQueSegue);
+            fwrite(&grau,1,sizeof(char),arquivo);
+
+            strcpy(grau,dados[i].dataFimQueSegue);
+            fwrite(&grau,1,sizeof(char),arquivo);
+
+        }
+  }
       
        
-    }
+    
 
 
     fclose(arquivo);
 
 }
-
