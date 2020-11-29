ff5ff12c711039f79e985a3be103900b49036935
CarlaNunes <carla.nunes@usp.br>
2020-11-28 21:12:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -20,39 +20,41 @@ int main() {
      int numAtu = 0;
      int retorno = 0;
     ///****Aqui começa a inserção dos valores de entrada para o manuseio dos arquivos
-    scanf("%d",&alternativa);
+    //scanf("%d",&alternativa);
     //printf("Valor da alternativa: %d",alternativa);
     scanf("%s",nomeArquivoCsv);
-    retorno = retirar_ponto(nomeArquivoCsv);
-    printf("\n Valor retorno = %d \n ",retorno);
-    if(retorno != 0){
+    //retorno = retirar_ponto(nomeArquivoCsv);
+   // printf("\n Valor retorno = %d \n ",retorno);
+    //if(retorno != 0){
        scanf("%s",nomeArquivoBinario);
-       scanf("%s",nomeArquivoIndice);
-    }
+      // scanf("%s",nomeArquivoIndice);
+   // }
    
 
     cont = le_arquivo(arquivo,nomeArquivoCsv); //Número de pessoas lidas
    // printf("pessoas lidas: %d\n",cont);
-    lidando_idade_negativas(arquivo,cont);
+    //lidando_idade_negativas(arquivo,cont);
     escreve_Binario2(arquivo,cont,nomeArquivoBinario);
    // le_index(arquivo,cont);
     //Criando preenchendo a struct de indice primário
-    for(i = 0; i < cont; i++){ 
+    
+   /*
+   for(i = 0; i < cont; i++){ 
       index_p[i].idPessoa = pessoa[i].idPessoa;
       index_p[i].rrn = i;
     }
-    
+    */
   //Função nativa do C, para ordenar os indices
-   qsort(index_p,cont,sizeof(struct index_p),comp);
+   //qsort(index_p,cont,sizeof(struct index_p),comp);
    //Criando o arquivo de indice primário depois da ordenação  
-   insereIndex(arquivo,cont,nomeArquivoIndice);
+   //insereIndex(arquivo,cont,nomeArquivoIndice);
    
    //Funcionalidade 1, CONFERIDA!
-   if(alternativa == 1){
-    atualizar_status(arquivo,cont,nomeArquivoBinario); 
-    binarioNaTela1(nomeArquivoBinario,nomeArquivoIndice);
+   //if(alternativa == 1){
+    //atualizar_status(arquivo,cont,nomeArquivoBinario); 
+    binarioNaTela2(nomeArquivoBinario);
     //exibir_campos(arquivo,cont,nomeArquivoBinario);
-   }  
+  // }  
   
     return (0);
 
