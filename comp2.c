a611a9b08e379cb0274c6bd4633fe7d6e90e33d3
CarlaNunes <carla.nunes@usp.br>
2020-11-30 10:47:39 -0300

    Update and rename comp.c to comp2.c

---
+++ comp2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<ctype.h>
+#include "funcoes.h"
+
+int comp2(const void *p, const void *q) {
+  struct dados *a = (struct dados*)p;
+  struct dados *b = (struct dados*)q;
+
+  if( a->idPessoaQueSegue > b->idPessoaQueSegue){
+    return 1;
+  }  
+  else if (b->idPessoaQueSegue < a->idPessoaQueSegue){
+    return -1;
+  }  
+  else if(b->idPessoaQueSegue == a->idPessoaQueSegue){
+       if( a->idPessoaQueESeguida > b->idPessoaQueESeguida)
+          return 1;
+      else if (b->idPessoaQueESeguida < a->idPessoaQueESeguida)
+        return -1;
+      else if(a->idPessoaQueESeguida == b->idPessoaQueESeguida){
+           if( a->dataInicioQueSegue > b->dataInicioQueSegue)
+             return 1;
+           else if (b->dataInicioQueSegue < a->dataInicioQueSegue)
+              return -1;
+             }
+           else
+              return 0;  
+         
+   }  
+  else
+    return 0;
+}
