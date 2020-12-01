45374244933f819451cd3ef8ae3d4bde7f3e7ea5
CarlaNunes <carla.nunes@usp.br>
2020-11-30 22:42:55 -0300

    Update comp.c

--- comp.c
+++ comp.c
@@ -7,13 +7,14 @@
 Irá ordenar de modo decrescente os id's para o uso posterior da função qsort.
 */
 int comp(const void *p, const void *q) {
-  struct index_p *a = (struct index_p*)p;
-  struct index_p *b = (struct index_p*)q;
+  //A função usa duas instancias para a varíavel id e as compara
+  struct index_p *a = (struct index_p*)p; //Primeira instãncia
+  struct index_p *b = (struct index_p*)q;//Segunda instância
 
   if( a->idPessoa > b->idPessoa )
-    return 1;
+    return 1; //Retorna 1, logo a será escrita
   else if (b->idPessoa < a->idPessoa)
-    return -1;
+    return -1; // Retorna -1, logo b será escrita
   else
-    return 0;
+    return 0;//Instâncias iguais
 }
