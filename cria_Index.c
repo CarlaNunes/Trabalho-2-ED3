aff42a0bd03b72f3e3d58dd9e5db1f3e3d8b8b49
CarlaNunes <carla.nunes@usp.br>
2020-11-30 22:48:58 -0300

    Update cria_Index.c

--- cria_Index.c
+++ cria_Index.c
@@ -6,38 +6,35 @@
 #include "funcoes.h"
 /*
 Cria o arquivo de índice primário.
+Recebe como parãmetros: ponteiro para arquivo,quantidade de pessoas lidas no arquivo csv e o nome que o arquivo de indice irá ter, respectivamente.
 */
 void cria_Index(FILE *arquivo,int cont,char nomeArquivoIndice[20]){
  
-  char status = '0'; //Arquivo de dados consistente. Ao finalizar terá valor igual a -1.
-  char lixo[] = "$$$$$$$";
-  char linha[400];
-  int id = 0;
+  char status = '0'; //Arquivo de dados consistente. Ao finalizar terá valor igual a -1.//Primeiro item do cabeçalho do arquivo index
+  char lixo[] = "$$$$$$$"; //Último item do cabeçalho do arquivo index
+  int id = 0; //Id auxiliar, para evitar possível impressão
   int aux = 0;
   //nt rRN = 0;
   int i = 0;
-  if ((arquivo = fopen(nomeArquivoIndice,"wb")) == NULL) {
+   if ((arquivo = fopen(nomeArquivoIndice,"wb")) == NULL) {
         printf("Erro ao abrir o arquivo.\n");
     }
   
   
   else{
-      //As duas linhas abaixo, escrevem o cabeçalho do arquivo de indíce primário.
       fwrite(&status, sizeof(char) , 1 , arquivo);
       fwrite(lixo, sizeof(char) , 1,  arquivo);
-      //Testando os valores recebidos:
-       for(i = 0; i < cont; i++){
-        // printf("\n ID: %d \n",index_p[i].idPessoa);
-         //printf("\n RRN: %d \n",index_p[i].rrn);
+      for(int i = 0; i<cont; i++){
+       //Impressão campo por campo, da struct index_p. Que irá conter os dados para gerar o arquivo de indice primário.
+        fwrite(&index_p.idPessoa,1, sizeof(Index_p),arquivo);
+        fwrite(&index_p.rrn,1, sizeof(Index_p),arquivo);
+        
        }
-     
-      for(i = 0; i < cont; i++){
-         fwrite(&pessoa,1, sizeof(Pessoa),arquivo);
-      }
-      
-      
+    
   }
+ fclose(arquivo);
 
-  fclose(arquivo);
-  
 }
+
+      
+  
